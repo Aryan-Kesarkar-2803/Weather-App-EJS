2600e275a0520685a2b02303ae8672d5807867ff
AryanKesarkar2803 <aryankesarkar0033@gmail.com>
2024-07-10 13:45:00 +0530

    commit 1

---
+++ models/users.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+// creating schema here
+const Schema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    city:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true
+    },
+},{timestamps:true})
+
+const users = mongoose.model("Users",Schema)
+
+module.exports = {
+    users,
+    mongoose,
+}
+
+
+
+
